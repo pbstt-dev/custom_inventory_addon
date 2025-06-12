124ba7fab75752655ebfac7f04050890deb32619
Danon Knox <danon.knox@pbstt.local>
2025-06-12 16:50:07 -0400

    Added hide_prices custom module under custom_addon folder

---
+++ custom_addon/hide_prices/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Hide Product Prices',
+    'version': '17.0.1.0.1',
+    'category': 'Inventory',
+    'summary': 'Hides sale and cost prices from product forms',
+    'author': 'Danon Knox',
+    'depends': ['product'],
+    'data': [
+        'views/hide_product_prices.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
